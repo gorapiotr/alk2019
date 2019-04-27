9ce2ad29b268468dfb3c2816f4bd8458e5fcd4b3
gorapiotr <pgora@sigma.ug.edu.pl>
2019-04-27 11:37:45 +0200

    Update zad 9

--- zad10/index.js
+++ zad10/index.js
@@ -2,4 +2,4 @@ var readlineSync = require('readline-sync');
 var sieve = require('./sieve.js');
 
 var n = readlineSync.question('Okresl tablice danych wejsciowych \n  np. [1, 2 , .... , N ] N: ');
-console.log(sieve.sieve(n));
+console.log('===WYNIK:===', sieve.sieve(n));
--- zad10/sieve.js
+++ zad10/sieve.js
@@ -17,27 +17,42 @@ module.exports = {
         return combos;
     },
 
-    sieve1: function (combinations, number) {
-        const comb = combinations.filter((item) => {
+    sieve1: function (array, number) {
+        const comb = this.combineWithoutRepetitions(array.filter((item, index) => {
+            return index < array.indexOf(number);
+        }), 2).filter((item) => {
             return item[0] + item[1] === number;
         });
-        return !!comb.length;
+
+        const response = !comb.length;
+        if (response) {
+            console.log('Sito 1 odrzuca: ', number);
+        }
+
+        return !comb.length;
     },
 
-    sieve2: function (combinations, number) {
-        return (combinations.filter((item) => {
+    sieve2: function (array, number) {
+        const comb = this.combineWithoutRepetitions(array.filter((item, index) => {
+            return index < array.indexOf(number);
+        }), 2).filter((item) => {
             return item[0] + item[1] === number;
-        }).length === 1);
+        });
+        const response = comb.length > 1;
+        if (response) {
+            console.log('Sito 2 odrzuca: ', number);
+        }
+
+        return response;
     },
 
-    sieve: function (arrayLastValue) {
-        const array = [1, 2];
+    sieve: function (arrayLength) {
+        let array = Array.from({length: arrayLength}, (v, k) => k + 1);
         let number = 3;
-        while (number <= arrayLastValue) {
-            let combinations = this.combineWithoutRepetitions(array, 2);
-            const isOk = this.sieve1(combinations, number) && this.sieve2(combinations, number);
-            if (isOk) {
-                array.push(number);
+        while (number < arrayLength) {
+            if (this.sieve1(array, number) || this.sieve2(array, number)) {
+                array.splice(array.indexOf(number), 1);
+                console.log('Aktualny stan:', array);
             }
             number++;
         }
